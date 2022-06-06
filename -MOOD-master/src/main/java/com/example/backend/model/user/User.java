f8772fe0cb4d311bdc7f977f5e9a085627920fbc
pearl-chae <76858949+pearl-chae@users.noreply.github.com>
2022-06-06 23:44:16 +0900

    2차수정

--- -MOOD-master/src/main/java/com/example/backend/model/user/User.java
+++ -MOOD-master/src/main/java/com/example/backend/model/user/User.java
@@ -1,18 +1,13 @@
 package com.example.backend.model.user;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
